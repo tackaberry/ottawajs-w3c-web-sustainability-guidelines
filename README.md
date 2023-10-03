a7abfc9a61e5f1102d4360caf4b72b773fd4fc23
tackaberry <tackaberry@google.com>
2023-10-03 13:51:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# W3C Web Sustainability Guidelines
+
+## Ottawa Javascript - Introducing W3C WSGs
+
+**Wednesday, October 11  @ 7 - 8PM**
+
+
+
+
+Slides deployed [here](http://ottawajs-w3c-web-sustainability-guidelines.s3-website.ca-central-1.amazonaws.com/).
+
+
+
+### Related
+
+- Brett is Principal Architect at [Google Cloud Canada](https://www.google.com)
+- This presentation is built with [MDX Deck](https://github.com/jxnblk/mdx-deck)
+
+
+
---
+++ src/components/Colours.js
@@ -0,0 +1,29 @@
+const ORANGE = "#db4437";
+const YELLOW = "#f4b400";
+const MAROON = "#6b393f";
+const BLUE = "#1f3442";
+const BRIGHT_BLUE = "#08b2e3";
+const GREEN = "#0f9d58";
+const LIGHT_GREEN = "#6cc24a";
+const BLACK = "#000";
+const WHITE = "#FFF";
+const LIGHT_GREY = "#fafafa";
+const DARK_GREY = "#333";
+const GREY = "#737373";
+const BEIGE = "#f9f7f1";
+
+export {
+  BEIGE,
+  ORANGE,
+  YELLOW,
+  MAROON,
+  BLUE,
+  BRIGHT_BLUE,
+  GREEN,
+  LIGHT_GREEN,
+  BLACK,
+  WHITE,
+  GREY,
+  LIGHT_GREY,
+  DARK_GREY,
+};
